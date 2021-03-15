480fb124eba71f22218b9196dbb8275d633e5fac
ur020202 <80566866+ur020202@users.noreply.github.com>
2021-03-15 17:12:45 +0900

    Add files via upload

---
+++ ch1_start.py
@@ -0,0 +1,17 @@
+# numeric data
+a=2
+b=3
+a+b
+# string
+s1 = "hello world!"
+print(s1)
+# data type : list
+x = [1,2,3]
+y = [4,5,6]
+x+y
+# function
+def add(a,b):
+    return a+b
+
+add(1,2)
+add(x,y)
