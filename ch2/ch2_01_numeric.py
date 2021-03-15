fc521369ff07366ee13a55a17e5e78f3421a815b
ur020202 <80566866+ur020202@users.noreply.github.com>
2021-03-15 17:54:33 +0900

    Add files via upload

---
+++ ch2/ch2_01_numeric.py
@@ -0,0 +1,42 @@
+# ch2_01_numeric.py
+#
+print("숫자형: 정수")
+a = 123
+print (a)
+a = -178
+print(a)
+a = 0
+print(a)
+print("숫자형: 실수")
+a = 1.2
+print (a)
+a = -3.48
+print(a)
+a = 4.24e10
+print(a)
+a = 4.24e-10
+print(a)
+print("숫자형: 8진수와 16진수")
+a = 0o177
+a
+a = 0x8FF
+a
+a = 0xABC
+a
+print("숫자형: 연산자와 연산")
+a = 3
+b = 4
+a + b
+a * b
+a / b
+a ** b
+a % b
+a // b
+
+14 // 3
+14 % 3
+
+"""
+Author: redwoods
+파이썬 코드: ch2_01_numeric.py
+"""
